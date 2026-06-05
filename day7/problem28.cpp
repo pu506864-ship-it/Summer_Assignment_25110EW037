a01795dc0332c4e7a9d1c18190d50a996a98b4e4
pu506864-ship-it <pu506864@gmail.com>
2026-06-05 11:23:45 +0530

    day7 complete

--- day7/problem28.cpp
+++ day7/problem28.cpp
@@ -1,8 +1,25 @@
 // Write a program to Recursive reverse number. 
 #include<iostream>
 using namespace std;
+int RevNum(int n,int ans);
 int main()
-{
+{int Num,ans=0;
+    cout<<"enter a number to reverse it";cin>>Num;
 
+ cout<<RevNum(Num,ans);
 return 0;
+}
+int RevNum(int n,int ans){
+    int digit;
+if(n==0) {
+    return ans;
+}
+    digit=n%10;
+    ans=ans*10+digit;
+    
+    n=n/10;
+    RevNum(n,ans);
+
+ 
+
 }
